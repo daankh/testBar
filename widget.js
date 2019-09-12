4920578efa38d3d7fc97b0c3c6afe964f74ed9db
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-12 07:46:34 +0200

    add show bar button to body

--- widget.js
+++ widget.js
@@ -2,5 +2,33 @@ class Bar {
     constructor(properties) {
         this.message = properties.message;
         this.position = properties.position;
+
+        const body = document.querySelector('body');
+        const bar = document.createElement('div');
+        const showBarBtn = document.createElement('button');
+
+        this.addShowBarBtn(showBarBtn, body);
+    }
+
+    addShowBarBtn(showBarBtn, place) {
+        showBarBtn.textContent = 'Show Bar';
+        //Center button start
+        showBarBtn.style.position = 'absolute';
+        showBarBtn.style.left = '50%';
+        showBarBtn.style.top = '50%';
+        showBarBtn.style.transform = 'translate(=50%,-50%)';
+        //Center button end
+        //Button styles start
+        showBarBtn.style.width = '80px';
+        showBarBtn.style.height = '80px';
+        showBarBtn.style.color = "#000";
+        showBarBtn.style.backgroundColor = "#fff"
+        showBarBtn.style.outline = 'none';
+        showBarBtn.style.border = "4px solid #888"
+        showBarBtn.style.borderRadius = "50%"
+        showBarBtn.style.cursor = "pointer";
+        //Button styles end
+
+        place.appendChild(showBarBtn);
     }
 }
