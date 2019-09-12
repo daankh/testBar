1018746d85f34423f85277320b6c78dafb404169
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-12 08:31:56 +0200

    add widget and widget elements without styles

--- widget.js
+++ widget.js
@@ -8,6 +8,7 @@ class Bar {
         const showBarBtn = document.createElement('button');
 
         this.addShowBarBtn(showBarBtn, body);
+        this.addBar(bar, body)
     }
 
     addShowBarBtn(showBarBtn, place) {
@@ -30,5 +31,45 @@ class Bar {
         //Button styles end
 
         place.appendChild(showBarBtn);
+    }
+
+    addBar = (bar, place) => {
+        //position bar START
+        bar.style.position = 'absolute';
+        bar.style.left = '0';
+        if (this.position === 'top') {
+            bar.style.top = '0';
+        } else if (this.position === 'bottom') {
+            bar.style.bottom = '0';
+        }
+        //position bar END
+        // bar styles START
+        bar.style.boxSizing = "border-box";
+        bar.style.width = '100%';
+        bar.style.height = '50px';
+        bar.style.backgroundColor = '#2c3e50';
+        bar.style.fontFamily = 'Arial, sans-serif';
+        bar.style.color = '#FFF';
+        // bar styles END
+
+        //text element START
+        const textEl = document.createElement('span');
+        textEl.textContent = this.message;
+        //textelemnt END
+
+        //get widgets btn START
+        const getWidgetsBtn = document.createElement('button')
+        getWidgetsBtn.textContent = 'Get widgets'
+        //get widgets btn END
+
+        //close btn START
+        const closeBtn = document.createElement('button')
+        closeBtn.textContent = 'X'
+        //close btn END
+
+        bar.appendChild(textEl);
+        bar.appendChild(getWidgetsBtn);
+        bar.appendChild(closeBtn);
+        place.appendChild(bar);
     }
 }
