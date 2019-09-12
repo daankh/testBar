308f7c11a69496410b1768b6d5b8c45bcc768a99
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-12 09:04:42 +0200

    add desktop bar styles

--- widget.js
+++ widget.js
@@ -17,7 +17,7 @@ class Bar {
         showBarBtn.style.position = 'absolute';
         showBarBtn.style.left = '50%';
         showBarBtn.style.top = '50%';
-        showBarBtn.style.transform = 'translate(=50%,-50%)';
+        showBarBtn.style.transform = 'translate(-50%,-50%)';
         //Center button end
         //Button styles start
         showBarBtn.style.width = '80px';
@@ -44,27 +44,47 @@ class Bar {
         }
         //position bar END
         // bar styles START
+        const barPadding = '4vw';
+        bar.style.padding = `0 ${barPadding}`;
         bar.style.boxSizing = "border-box";
         bar.style.width = '100%';
         bar.style.height = '50px';
         bar.style.backgroundColor = '#2c3e50';
         bar.style.fontFamily = 'Arial, sans-serif';
         bar.style.color = '#FFF';
+        bar.style.display = "flex";
+        bar.style.alignItems = "center";
+        bar.style.justifyContent = "center";
         // bar styles END
 
         //text element START
         const textEl = document.createElement('span');
         textEl.textContent = this.message;
-        //textelemnt END
+        //text element END
 
         //get widgets btn START
         const getWidgetsBtn = document.createElement('button')
-        getWidgetsBtn.textContent = 'Get widgets'
+        getWidgetsBtn.textContent = 'Get widgets';
+        getWidgetsBtn.style.color = 'inherit';
+        getWidgetsBtn.style.backgroundColor = '#f39c12';
+        getWidgetsBtn.style.outline = 'none';
+        getWidgetsBtn.style.border = 'none';
+        getWidgetsBtn.style.borderRadius = '4px';
+        getWidgetsBtn.style.padding = '.5em 1em';
+        getWidgetsBtn.style.cursor = 'pointer';
+        getWidgetsBtn.style.margin = " 0 1em";
         //get widgets btn END
 
         //close btn START
-        const closeBtn = document.createElement('button')
-        closeBtn.textContent = 'X'
+        const closeBtn = document.createElement('button');
+        closeBtn.textContent = 'X';
+        closeBtn.style.outline = 'none';
+        closeBtn.style.border = 'none';
+        closeBtn.style.color = 'inherit';
+        closeBtn.style.backgroundColor = 'transparent';
+        closeBtn.style.cursor = 'pointer';
+        closeBtn.style.position = 'absolute';
+        closeBtn.style.right = `${barPadding}`
         //close btn END
 
         bar.appendChild(textEl);
